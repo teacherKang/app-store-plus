af6aa0c86ec107222b69b7f23ca5d998f437dc4a
4434 <root@DESKTOP-DAT3T20.localdomain>
2019-09-19 09:35:07 +0800

    fix

---
+++ src/login/login.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../style/login/login.scss';
+class Documentation extends Component {
+  state = {
+    
+  };
+  render() {
+    const {Documentations} = this.state;
+    return (
+      <div className="lgoin-box">
+          
+      </div>
+    );
+  }
+}
+
+
+export default Documentation;
